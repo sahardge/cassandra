5938032ecc085875950e82ade8bff26b4f091d1c
sahardge <sahardge1@catamount.wcu.edu>
2014-06-04 14:34:33 -0400

    Update start-cassandra.sh

--- start-cassandra.sh
+++ start-cassandra.sh
@@ -1,5 +1,5 @@
-IP=ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'
+IP=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 
 cat /etc/cassandra/cassandra.yaml.orig | sed -e "112,197 s|/var/lib|/mountedvol|g" | sed -e "329 s/localhost/""$IP""/" | sed -e "365 s/localhost/""$IP""/" | sed -e "238 s/127.0.0.1/""$IP""/" > /etc/cassandra/cassandra.yaml
-cassandra -f
+/usr/sbin/cassandra -f
 
