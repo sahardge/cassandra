26aa772ce39f9a63c8034eeceec2ed4b3b2df569
sahardge <sahardge1@catamount.wcu.edu>
2014-06-04 12:15:43 -0400

    Update rexster.sh

--- rexster.sh
+++ rexster.sh
@@ -1,5 +1,4 @@
 echo "Installing Rexster..."
-wget -q http://tinkerpop.com/downloads/rexster/rexster-server-2.5.0.zip
-unzip rexster-serverr-2.5.0.zip -d /tmp/rexster
+wget -q http://tinkerpop.com/downloads/rexster/rexster-server-2.5.0.zip && unzip rexster-serverr-2.5.0.zip -d /tmp/rexster
 mv /tmp/rexster/rexster-server-2.5.0/ /rexster
 cd rexster
