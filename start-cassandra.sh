0f2d86610fa73eb19d5b756e5e28b49d15372920
sahardge <sahardge1@catamount.wcu.edu>
2014-06-06 13:42:29 -0400

    Delete start-cassandra.sh

--- start-cassandra.sh
+++
@@ -1,5 +0,0 @@
-IP=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
-
-cat /etc/cassandra/cassandra.yaml.orig | sed -e "112,197 s|/var/lib|/mountedvol|g" | sed -e "329 s/localhost/""$IP""/" | sed -e "365 s/localhost/""$IP""/" | sed -e "238 s/127.0.0.1/""$IP""/" > /etc/cassandra/cassandra.yaml
-/usr/sbin/cassandra -f
-
