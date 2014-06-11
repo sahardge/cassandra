c0b92eafa5d5fa2f703becb62313ead689315ae8
sahardge <sahardge1@catamount.wcu.edu>
2014-06-11 11:07:46 -0400

    Update install-cassandra.sh

--- install-cassandra.sh
+++ install-cassandra.sh
@@ -17,7 +17,7 @@ echo "Installing Cassandra 1.2 branch..."
   echo "Cassandra has been installed."
   echo "Making appropriate configuration changes."
   mv /etc/cassandra/cassandra.yaml /etc/cassandra/cassandra.yaml.orig 
-  cat /etc/cassandra/cassandra.yaml.orig | sed -e "112,197 s|/var/lib|/mountedvol|g" | sed -e "329 s/localhost/""$IP""/" | sed -e "365 s/localhost/0.0.0.0/" | sed -e "238 s/127.0.0.1/172.17.42.1/" > /etc/cassandra/cassandra.yaml
+  cat /etc/cassandra/cassandra.yaml.orig | sed -e "112,197 s|/var/lib|/mountedvol|g" | sed -e "329 s/localhost/""$IP""/" | sed -e "365 s/localhost/0.0.0.0/" | sed -e "238 s/127.0.0.1/""$IP""/" > /etc/cassandra/cassandra.yaml
   cp start-cass.sh /usr/sbin
   cp ipof.sh /usr/sbin
   
