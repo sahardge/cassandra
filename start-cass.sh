123a29a16890b194443c7aa0eebae8c3d160e412
sahardge <sahardge1@catamount.wcu.edu>
2014-06-11 11:14:43 -0400

    Update start-cass.sh

--- start-cass.sh
+++ start-cass.sh
@@ -14,3 +14,5 @@ sed -i -e "s/^listen_address.*/listen_address: $IP/" $CONFIG/cassandra.yaml
 sed -i -e "s/^rpc_address.*/rpc_address: 0.0.0.0/" $CONFIG/cassandra.yaml
 sed -i -e "s/- seeds: \"127.0.0.1\"/- seeds: \"$SEEDS\"/" $CONFIG/cassandra.yaml
 sed -i -e "s/# JVM_OPTS=\"$JVM_OPTS -Djava.rmi.server.hostname=<public name>\"/ JVM_OPTS=\"$JVM_OPTS -Djava.rmi.server.hostname=$IP\"/" $CONFIG/cassandra-env.sh
+
+cassandra -f
