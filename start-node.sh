7b06d53d7cf709ae1f75119aca991e22a80d4bfe
sahardge <sahardge1@catamount.wcu.edu>
2014-06-12 15:41:08 -0400

    Update start-node.sh

--- start-node.sh
+++ start-node.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 IP=`hostname --ip-address`
-SEEDS=/bin/etcdctl get /seed
+SEEDS=/bin/etcdctl --peers 10.1.42.1 get /seed
 
 
 echo Configuring Cassandra to listen at $IP with seeds $SEEDS
@@ -14,5 +14,6 @@ sed -i -e "s/^listen_address.*/listen_address: $IP/" $CONFIG/cassandra.yaml
 sed -i -e "s/^rpc_address.*/rpc_address: 0.0.0.0/" $CONFIG/cassandra.yaml
 sed -i -e "s/- seeds: \"127.0.0.1\"/- seeds: \"$SEEDS\"/" $CONFIG/cassandra.yaml
 sed -i -e "s/# JVM_OPTS=\"$JVM_OPTS -Djava.rmi.server.hostname=<public name>\"/ JVM_OPTS=\"$JVM_OPTS -Djava.rmi.server.hostname=$IP\"/" $CONFIG/cassandra-env.sh
+
 
 cassandra -f
