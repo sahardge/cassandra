c2575423416fb2eac6dcbb1f6c7d110cd81bc8a5
sahardge <sahardge1@catamount.wcu.edu>
2014-06-12 14:34:14 -0400

    Create install-etcd.sh

---
+++ install-etcd.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget -q https://github.com/coreos/etcd/releases/download/v0.4.0/etcd-v0.4.0-darwin-amd64.zip && unzip etcd-v0.4.0-darwin-amd64.zip -d /tmp/etcd
+mv /tmp/etcd/etcd-v0.4.0-darwin-amd64.zip/etcd /usr/bin && mv /tmp/etcd/etcd-v0.4.0-darwin-amd64.zip/etcdctl /usr/bin
