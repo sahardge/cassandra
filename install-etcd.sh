13efb1795fc0d5d31267d6c01c43a6a4047a6614
sahardge <sahardge1@catamount.wcu.edu>
2014-07-02 14:44:30 -0400

    Update install-etcd.sh

--- install-etcd.sh
+++ install-etcd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir /tmp/etcd
-wget -q https://github.com/coreos/etcd/releases/download/v0.4.3/etcd-v0.4.3-linux-amd64.tar.gz && tar -xvf etcd-v0.4.3-linux-amd64.tar.gz -C /tmp/etcd
-mv /tmp/etcd/etcd-v0.4.3-linux-amd64/etcd /usr/bin && mv /tmp/etcd/etcd-v0.4.3-linux-amd64/etcdctl /usr/bin
+wget -q https://github.com/coreos/etcd/releases/download/v0.4.4/etcd-v0.4.4-linux-amd64.tar.gz && tar -xvf etcd-v0.4.4-linux-amd64.tar.gz -C /tmp/etcd
+mv /tmp/etcd/etcd-v0.4.4-linux-amd64/etcd /usr/bin && mv /tmp/etcd/etcd-v0.4.4-linux-amd64/etcdctl /usr/bin
